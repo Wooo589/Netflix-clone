65f1578fa85f35f59bc1df2f8bfe8e329cbcd8d5
Willian Silva <75449306+Wooo589@users.noreply.github.com>
2022-05-14 02:01:10 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/*  Operações dinâmicas */
+
+const assistir = () => {
+    return window.open('https://www.youtube.com/watch?v=m4gnMWua4xo', '_blank');
+}
+
+const inicio = () => {
+    return location.href = "#inicio";
+}
